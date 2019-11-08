13603da1a0dc763a77a19278853b63a5f0622056
Carlos Fortin <cafortinl@hotmail.com>
2019-11-08 13:58:45 -0600

    Creacion de los 5 administradores y los 5 libros predeterminados

--- src/examen/pkg1_carlosfortin/Principal.java
+++ src/examen/pkg1_carlosfortin/Principal.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package examen.pkg1_carlosfortin;
 
 /**
  *
  * @author Carlos Fortin
  */
+import java.util.ArrayList;
+import java.util.Date;
 public class Principal extends javax.swing.JFrame {
 
     /**
@@ -16,6 +13,17 @@ public class Principal extends javax.swing.JFrame {
      */
     public Principal() {
         initComponents();
+        
+        usuarios.add(new Administrador("Admin1", "1234", new Date(), "3333-2222", "admin1@hotmail.com", "Accion"));
+        usuarios.add(new Administrador("Admin2", "4321", new Date(), "2222-3333", "admin2@gmail.com", "Fantasia"));
+        usuarios.add(new Administrador("Admin3", "0000", new Date(), "2828-2727", "admin3@yahoo.com", "Historia"));
+        usuarios.add(new Administrador("Admin4", "1111", new Date(), "2234-2221", "admin4@hotmail.com", "Romance"));
+        usuarios.add(new Administrador("Admin5", "password", new Date(), "3223-2425", "admin5@hotmail.com", "Accion"));
+        libros.add(new Libro("IT", "Un niño es asesinado en el pequeño pueblo de Derry, y William Denbrough y sus amigos empiezan a descubrir el extraño misterio de este pueblo", 4, 7, "Fantasia", 340.59, 2, "Stephen King", 1986));
+        libros.add(new Libro("El Arbol de Huesos", "Un joven descubre que su realidad no es la correcta", 4, 5, "Fantasia", 120, 1, "Anonimo", 2017));
+        libros.add(new Libro("Desconocido", "Una joven llega a casa y descubre que no esta sola", 3, 8, "Accion", 150.60, 3, "Victor Escalante", 2001));
+        libros.add(new Libro("Clasificado", "Un detective descubre un gran secreto relacionado con una figuta muy importante de su ciudad", 4, 2, "Historia", 230, 4, "Anonimo", 2018));
+        libros.add(new Libro("Si supieras", "Un buen ejemplo de un romance de secundaria", 2, 11, "Romance", 175.22, 2, "Fernanda Mendoza", 2012));
     }
 
     /**
@@ -119,4 +127,7 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JTabbedPane jTabbedPane1;
     // End of variables declaration//GEN-END:variables
+    ArrayList<Persona> usuarios=new ArrayList();
+    ArrayList<Libro> libros=new ArrayList();
+
 }
